72a3b8d990a60edc8a17d651ec35db00b63b965a
kantaroNakaya <tribestaff0105@gmail.com>
2025-05-27 18:53:58 +0900

    求人サイト　背景の境界を修正

--- recruit/index.php
+++ recruit/index.php
@@ -1,16 +1,25 @@
 <?php include $path . 'components/head.php'; ?>
 <div class="bg bg01" id="top">
-  <?php include $path . 'components/header.php'; ?>
-  <?php include $path . './components/home.php'; ?>
+  <div class="bg__inner bg01__inner">
+    <?php include $path . 'components/header.php'; ?>
+    <?php include $path . './components/home.php'; ?>
+
+  </div>
 </div>
 <div class="bg bg02">
-  <?php include $path . './components/about.php'; ?>
+  <div class="bg__inner bg02__inner">
+    <?php include $path . './components/about.php'; ?>
+  </div>
 </div>
 <div class="bg bg03">
-  <?php include $path . './components/salary.php'; ?>
+  <div class="bg__inner bg03__inner">
+    <?php include $path . './components/salary.php'; ?>
+  </div>
 </div>
 <div class="bg bg04">
-  <?php include $path . './components/assurance.php'; ?>
+  <div class="bg__inner bg04__inner">
+    <?php include $path . './components/assurance.php'; ?>
+  </div>
 </div>
 <div class="bg bg05">
   <?php include $path . './components/contact.php'; ?>
